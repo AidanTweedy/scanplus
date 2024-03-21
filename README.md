489406c3b4aea2b8c69997ad6dc471f0f3b8fa5a
Aidan <adtweedy@gmail.com>
2024-03-21 01:15:46 -0400

    Initial release 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# LethalCompanyTemplate
-A template for a BepInEx 5 Plugin, using the correct .csproj configuration for Lethal Company modding
+# ScanPlus
+
+[![GitHub Build Status](https://img.shields.io/github/actions/workflow/status/AidanTweedy/scanplus/build.yml?style=for-the-badge&logo=github)](https://github.com/AidanTweedy/scanplus/actions/workflows/build.yml)
+
+A mod for *Lethal Company* which upgrades the ship's scanner, allowing it to scan lifeforms in addition to scrap.
+
+## Installation
+
+Place `ScanPlus.dll` in your `BepInEx/plugins` folder.
+
+Alternatively, you can use Thunderstore Mod Manager.
+
+## Usage
+
+To use the upgraded scanner the player must purchase the `Infrared Scanner` ship upgrade. Doing so will add information about which creatures are spawned on the current moon.
+
+The need for the `Infrared Scanner` upgrade can be disabled in the config file (see below), allowing for the enhanced scan without having to purchase the upgrade.
+
+## Configuration
+
+`ScanPlus.cfg` will be generated after runnning *Lethal Company* for the first time after installing this mod, allowing for several changes to be made:
+
+* Switch between `Low`, `Medium`, `High`, and `Excessive` levels of detail in the upgraded scan messages. 
+
+* Enable/Disable need for ship upgrade to use enhanced scanner.
+
+* Adjust price of the `Infrared Scanner` ship upgrade (if enabled).
+
+## Bugs/Troubleshooting/Suggestions
+
+Found a bug, encountered a problem, or have a suggestion? - let us know on [GitHub!](https://github.com/AidanTweedy/scanplus/issues)
---
+++ ScanPlus/ScanPlus.cs
@@ -0,0 +1,138 @@
+﻿﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
+using LethalLib.Modules;
+
+using TerminalApi.Events;
+
+namespace ScanPlus
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("atomic.terminalapi")]
+    public class ScanPlus : BaseUnityPlugin
+    {
+        private static ManualLogSource _log = null!;
+        private static ConfigEntry<int>
+            config_DetailLevel,
+            config_ShipUpgrade;
+        private static int m_detailLevel;
+        private static bool m_shipUpgrade;
+
+        private static Unlockables.RegisteredUnlockable scanner;
+
+        private const string DefaultString = "\nNo life detected.\n\n";
+
+        private const string UpgradeName = "Infrared Scanner";
+        private const string UpgradeInfo = "\nUpgrades the ship's scanner with an infrared sensor, allowing for the detection of lifeforms present on the current moon.\n";
+
+        private void Awake()
+        {
+            _log = Logger;
+
+            ConfigFile();
+
+            Logger.LogInfo($"{PluginInfo.PLUGIN_GUID} is loaded");
+
+            Events.TerminalParsedSentence += OnTerminalParsedSentence;
+
+            if (!m_shipUpgrade)
+                return;
+
+            var scanUpgrade = new UnlockableItem
+            {
+                unlockableName = UpgradeName,
+                spawnPrefab = false,
+                alwaysInStock = true,
+                prefabObject = null,
+                unlockableType = 1,
+                IsPlaceable = false,
+                maxNumber = 1,
+                canBeStored = false,
+                alreadyUnlocked = false
+            };
+
+            Unlockables.RegisterUnlockable(scanUpgrade , storeType: StoreType.ShipUpgrade, price: 1);
+            scanner = Unlockables.registeredUnlockables.Find(u => u.unlockable.unlockableName == UpgradeName);
+            
+
+            TerminalNode infoNode = TerminalApi.TerminalApi.CreateTerminalNode(displayText: UpgradeInfo, clearPreviousText: true);
+            
+            scanner.itemInfo = infoNode;
+        }
+
+        private void ConfigFile()
+        {
+            //Setup Config File
+            config_DetailLevel = Config.Bind("DETAIL", "DetailLevel", 2, "Life Scan Detail -> | 0 = LOW | 1 = MEDIUM | 2 = HIGH | 3 = EXCESSIVE");
+            config_ShipUpgrade = Config.Bind("PREFERENCES", "ShipUpgrade", 1, "Enable Ship Upgrade -> | 0 = false | 1 = true");
+
+            //Load Config File
+            m_detailLevel = config_DetailLevel.Value;
+            m_shipUpgrade = Convert.ToBoolean(config_ShipUpgrade.Value);
+        }
+
+        private static void OnTerminalParsedSentence(object sender, Events.TerminalParseSentenceEventArgs e)
+        {
+            if (e.ReturnedNode.name != "ScanInfo")
+                return;
+
+            if (m_shipUpgrade && !scanner.unlockable.hasBeenUnlockedByPlayer)
+                return;
+
+            e.ReturnedNode.displayText = e.ReturnedNode.displayText.Split('\n')[0] + '\n' + BuildEnemyCountString();
+        }
+
+        private static string BuildEnemyCountString()
+        {
+            var entities = FindObjectsOfType<EnemyAI>()
+                .Where(ai => ai.GetComponentInChildren<ScanNodeProperties>() is not null)
+                .GroupBy(ai => ai.enemyType.enemyName)
+                .OrderBy(g => g.Key)
+                .ToList();
+
+            if (entities.Count == 0)
+                return DefaultString;
+
+            StringBuilder sb = new();
+            sb.Append('\n');
+
+            int numEntities = RoundManager.Instance.numberOfEnemiesInScene;
+            int threatLevel = RoundManager.Instance.currentEnemyPower;
+            int maxThreatLevel = RoundManager.Instance.currentLevel.maxEnemyPowerCount;
+            string relativeThreat = $"{(float)threatLevel / maxThreatLevel * 100:N1}%";
+
+            switch (m_detailLevel)
+            {
+                case 0:
+                    sb.Append($"Threat level: {relativeThreat}");
+                    break;
+                case 1:
+                    sb.Append($"{numEntities} lifeforms have been detected, totalling a threat level of {threatLevel}.");
+                    break;
+                case 2:
+                    sb.Append("Life detected:");
+                    foreach (var group in entities)
+                    {
+                        sb.AppendLine($"\n  {group.Key}: {group.Count()}");
+                    }
+                    break;
+                case 3:
+                    sb.Append($"Threat level: {relativeThreat}\n\n{numEntities} lifeforms detected, totalling a threat level of {threatLevel}.\n\nLife detected:");
+                    foreach (var group in entities)
+                    {
+                        sb.AppendLine($"\n  {group.Key}: {group.Count()}");
+                    }
+                    break;
+            }
+
+            sb.AppendLine();
+            return sb.ToString();
+        }
+    }
+}
