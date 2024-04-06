95d7c1acbe62dbbca7396498b2c2782bee8cfbc3
Aidan <adtweedy@gmail.com>
2024-04-05 21:56:19 -0400

    minor update

--- ScanPlus/Patches/TerminalFormatter.cs
+++ ScanPlus/Patches/TerminalFormatter.cs
@@ -5,7 +5,7 @@ namespace ScanPlus
     [HarmonyPatch(typeof(Terminal))]
     public class TFCompatibility
     {
-        [HarmonyPatch("TextPostProcess")]
+        [HarmonyPostfix, HarmonyPatch("TextPostProcess")]
         public static void TextPostProcessPrefixPostFix(string modifiedDisplayText, TerminalNode node, Terminal __instance)
         {
             if (node.name == "ScanInfo" && ScanPlus.Instance._scanner.UseUpgradedScan())
